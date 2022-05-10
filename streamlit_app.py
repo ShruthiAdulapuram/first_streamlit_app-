85610c2e45a76e8a2ebad9fd7e1f0d8dd8782e60
Shruthi Adulapuram <105211708+ShruthiAdulapuram@users.noreply.github.com>
2022-05-10 13:39:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 &  Bluberry Oatmeal')
+streamlit.text('🥣 Omega 3 &  Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
